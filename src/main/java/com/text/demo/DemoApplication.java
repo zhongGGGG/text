e69aa1b959a52288e6fe69445ace3bb70ec86b08
e <zhongGGGG>
2020-03-25 16:35:21 +0800

    测试

--- src/main/java/com/text/demo/DemoApplication.java
+++ src/main/java/com/text/demo/DemoApplication.java
@@ -20,4 +20,14 @@ public class DemoApplication {
             }
         }
     }
+    public static  class  test2{
+        public static void main(String[] args){
+            for(int i=1;i<=9;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j+"*"+i+"="+i*j+'\t');
+                }
+                System.out.println();
+            }
+        }
+    }
 }
